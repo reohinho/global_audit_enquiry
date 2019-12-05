7286c130896e8025dd7792b51c855c6d1dc19d8e
Leo Ching <reohinho@gmail.com>
2019-12-05 12:03:25 +0800

    init commit

---
+++ src/main/java/hk/org/ha/pbrc/globalauditenquiry/GlobalAuditEnquiryApplication.java
@@ -0,0 +1,13 @@
+package hk.org.ha.pbrc.globalauditenquiry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalAuditEnquiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalAuditEnquiryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hk/org/ha/pbrc/globalauditenquiry/GlobalAuditEnquiryApplicationTests.java
@@ -0,0 +1,13 @@
+package hk.org.ha.pbrc.globalauditenquiry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalAuditEnquiryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
